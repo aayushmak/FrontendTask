6448d6ee2566406c23571114193acbeae31d2b84
aayushmak <aayushmakafle@gmail.com>
2025-07-12 23:01:53 +0545

    -

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -47,7 +47,7 @@ const Card = ({ isRed, onClick }) => {
       onClick={onClick}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      className={`relative ${isRed ? 'w-[592px] h-[461px] px-16 py-10' : 'w-[294px] h-[472px] p-6'} 
+      className={`relative ${isRed ? 'w-[592px] h-[461px] px-16 py-10' : 'w-[294px] h-[461px] p-6'} 
                   rounded-[30px] flex flex-col justify-between items-center mt-24  cursor-pointer`}
       style={{ fontFamily: 'Nohemi', backgroundColor: isRed ? '#c62828' : '#fde2e2' }}
     >
@@ -71,7 +71,7 @@ const Card = ({ isRed, onClick }) => {
       </div>
 
       {/* Vertical text for pink */}
-      <div ref={verticalTextRef} className="flex flex-col items-center rotate-[-90deg] mt-6">
+      <div ref={verticalTextRef} className="flex flex-col items-center rotate-[-90deg] -mt-4">
         <h2 className="text-red-700 text-4xl font-bold">Upcoming Courses</h2>
         <p className="text-red-700 text-lg mt-2 text-center leading-4 max-w-[180px]">
           exciting new courses waiting to boost your skills.
@@ -92,7 +92,7 @@ const Card = ({ isRed, onClick }) => {
 
       {/* Bottom number for pink */}
       {!isRed && (
-        <div className="flex items-center -mt-24">
+        <div className="flex items-center -mt-4">
           <h1 className="text-[150px] font-bold text-red-700 leading-none">05</h1>
           <img src={redPlus} alt="plus" className="-mt-28 w-9 h-auto mb-4" />
         </div>
--- src/pages/PageThree.jsx
+++ src/pages/PageThree.jsx
@@ -5,10 +5,10 @@ const PageThree = () => {
   const [activeRedIndex, setActiveRedIndex] = useState(0); // first card red initially
 
   return (
-    <main className='justify-center p-10 h-screen'>
+    <main className=' p-10 px-15'>
       <p className='text-2xl text-nowrap text-medium'>Explore our classes and master trending skills!</p>
             <p className='text-4xl text-black font-bold'>Skill <span className='text-green-700'>Dive Into </span> What’s Hot Right Now! 🔥</p>
-      <div className="flex gap-4 bottom-0 ">
+      <div className="flex justify-center mb-5 gap-4 bottom-0 ">
       {[0,1,2].map((index) => (
         <Card
           key={index}
